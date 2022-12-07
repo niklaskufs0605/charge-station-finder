36034fcf7f2455d57408eba261b9c7174df59eda
niklaskufs0605 <niklas.kufs@otto.de>
2022-12-07 18:03:06 +0100

    Create Repository

---
+++ src/main/java/com/niklas/chargestationfinder/ChargeStationFinderApplication.java
@@ -0,0 +1,13 @@
+package com.niklas.chargestationfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeStationFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChargeStationFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niklas/chargestationfinder/ChargeStationFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niklas.chargestationfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeStationFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
