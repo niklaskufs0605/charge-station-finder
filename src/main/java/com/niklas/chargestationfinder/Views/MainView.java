cb843493d0a30159c7291e12988abb13504cba64
niklaskufs0605 <niklas.kufs@otto.de>
2022-12-07 18:07:44 +0100

    Added MainView

---
+++ src/main/java/com/niklas/chargestationfinder/Views/MainView.java
@@ -0,0 +1,11 @@
+package com.niklas.chargestationfinder.Views;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.upload.Upload;
+
+public class MainView extends Div {
+    public MainView() {
+        add(new Button("Test"));
+    }
+}
